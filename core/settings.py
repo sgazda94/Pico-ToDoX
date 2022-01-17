bfa658e436dfb20c17426c3e1f591f9153a9e546
sgazda94 <gazda94@gmail.com>
2022-01-17 23:40:47 +0100

    allowed hosts added

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-o&j(wcnhw338%$ewhbl@as4cr*!s#za6hhyr=z))i^#vt#d)-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pico-todox.herokuapp.com']
+ALLOWED_HOSTS = ['pico-todox.herokuapp.com','localhost'
+'127.0.0.1']
 
 
 # Application definition
